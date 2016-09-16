87cb0935d041085a31c6b2df1cbeb802f9bd5551
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-09-16 17:15:54 +0900

    1W3−4_日付追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,3 +9,18 @@ module ApplicationHelper
     link_to_unless_current(text, path) { content_tag(:span, text) }
   end
 end
+
+def today_with_wday
+  wdays = %w(日 月 火 水 木 金 土)
+  t = Time.current.advance(days: 2) #.advance(days: 2)は２日後を示す
+  s = t.strftime("%Y年%-m月%-d日") #-は０を無くす
+  case t.wday
+  when 0
+    s << "(<span style='color: red'>#{wdays[t.wday]}</span>)"
+  when 6
+    s << "(<span style='color: blue'>#{wdays[t.wday]}</span>)"
+  else
+    s << "(#{wdays[t.wday]})"
+  end
+  s.html_safe #エスケープを無効化
+end
