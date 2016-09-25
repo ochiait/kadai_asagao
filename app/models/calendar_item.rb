bec29cffd06c15cf31fb6f653571661c0ff42df9
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-09-25 17:01:55 +0900

    4W予-2_バリデーション

--- app/models/calendar_item.rb
+++ app/models/calendar_item.rb
@@ -1,2 +1,10 @@
 class CalendarItem < ApplicationRecord
+  validates :title, presence: true,
+    length: { maximum: 20}
+
+  validate :start_time_check
+
+  def  start_time_check
+    errors.add( :start_time, :invalid_calendar_item_start_time ) unless start_time <= end_time
+  end
 end
